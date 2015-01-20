e08c99c2a0a2e667649fb30fc0de92d6593f9f0f
bibiana <bcristofol@gmail.com>
2015-01-20 17:25:04 +0000

    Temperature can be reseted to 20

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -30,6 +30,12 @@ describe("Thermostat", function() {
       expect(thermostat.temperature).toEqual(10);
     });
 
+    it("can be reset to 20 by hitting the reset button", function() {
+      thermostat.changeTemperature(-1);
+      thermostat.resetTemperature();
+      expect(thermostat.temperature).toEqual(20);
+    });
+
   });
 
 });
